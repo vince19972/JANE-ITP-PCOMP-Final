089c7be7cc4c2257f15ba596fe5e1d69edfc80ed
Vince Shao <vinceshao1992@gmail.com>
2018-11-29 19:47:45 -0500

    state machine condition added

--- servo-test/servo-test.ino
+++ servo-test/servo-test.ino
@@ -1,28 +1,102 @@
 #include <Servo.h>
 #include <SoftwareSerial.h>
 
-/* 
- * global store
+/*
+    global store
 */
 const int modulesNum = 3;
 int insertedTabletsNum = 0;
+boolean isRunning = true; // true if user is here
+boolean isActivatingStage = true;
+String currentStage = "sleeping";
 
 void update_insertedTabletsNum(boolean toIncrease) {
   boolean tabletsAllIn = (insertedTabletsNum == modulesNum);
   boolean tabletsAllOut = (insertedTabletsNum == 0);
   boolean readyToIncrease = (toIncrease && insertedTabletsNum < modulesNum && !tabletsAllIn);
   boolean readyToDecrease = (!toIncrease && insertedTabletsNum > 0 && !tabletsAllOut);
-  
+
   if (readyToIncrease) insertedTabletsNum -= 1;
   if (readyToDecrease) insertedTabletsNum += 1;
 };
 
+String update_stage() {
+  if (isRunning) {
+    isActivatingStage = true;
+    
+    if (isActivatingStage) {
+      if (currentStage != "activating_final") {
+        switch (insertedTabletsNum) {
+          case 0:
+            currentStage = "sleeping";
+            // do the actions...
+          case 1:
+            currentStage = "regular_1";
+            // do the actions...
+          case 4:
+            currentStage = "regular_2";
+            // do the actions...
+          case 6:
+            currentStage = "regular_3";      
+            // do the actions...
+          case 9:
+            currentStage = "activating_final";            
+            // do the actions...
+        }         
+      } else {
+        switch (insertedTabletsNum) {
+          case 8:
+            currentStage = "deactivated_regular";
+            // do the actions...
+            isRunning = false;
+          case 10:
+            // do the actions...
+            isActivatingStage = false;
+      }    
+    } else {
+      if (currentStage != "deactivating_final") {
+        switch (insertedTabletsNum) {   
+          case 10:
+            currentStage = "advanced_1";  
+            // do the actions...
+          case 9:
+            currentStage = "advanced_2";  
+            // do the actions...
+          case 6:
+            currentStage = "advanced_3";  
+            // do the actions...
+          case 4:
+            currentStage = "advanced_4";  
+            // do the actions...
+          case 2:
+            currentStage = "advanced_5"; 
+            // do the actions...
+          case 1:
+            currentStage = "deactivating_final";
+            // do the actions...
+        }           
+      } else {
+        switch (insertedTabletsNum) {
+          case 1:
+            currentStage = "ending_advanced";
+            // do the actions...
+            isRunning = false;
+          case 0:
+            currentStage = "deactivated_advanced";          
+            // do the actions...
+            isRunning = false;
+      }
+  } else {  
+    currentStage = "sleeping";
+  }
+};
+
 
 /*
- * classes 
- */
+   classes
+*/
 class ModuleSet {
-  public: 
+  public:
     ModuleSet(Servo servo, int btnPin);
     void updateBtnState();
     void moveTablet();
@@ -30,15 +104,15 @@ class ModuleSet {
     boolean tableIsOut = false;
   private:
     // base vars
-    Servo _servo;  
+    Servo _servo;
     int _ModuleSetIndex;
     int _btnPin;
     int _btnRead = 0;
 
     // btn toggle vars
     int _btnWasOn = !btnIsOn;
-    unsigned long _time = 0;           
-    unsigned long _debounce = 200UL;     
+    unsigned long _time = 0;
+    unsigned long _debounce = 200UL;
 };
 
 ModuleSet::ModuleSet(Servo servo, int btnPin) {
@@ -65,27 +139,28 @@ void ModuleSet::updateBtnState() {
 
 void ModuleSet::moveTablet() {
   updateBtnState();
-//  Serial.println(btnIsOn);
   Serial.println(insertedTabletsNum);
-  
+
   if (btnIsOn)
     _servo.write(180);
-  else 
+  else
     _servo.write(0);
+
+  //   update_insertedTabletsNum();
 }
 
 /*
- * init settings 
- */
- 
+   init settings
+*/
+
 /* servos */
 const int servoStartNum = 21;
 
-Servo servo_1; 
+Servo servo_1;
 const int servo_1_pin = servoStartNum + 0;
-Servo servo_2; 
+Servo servo_2;
 const int servo_2_pin = servoStartNum + 1;
-Servo servo_3; 
+Servo servo_3;
 const int servo_3_pin = servoStartNum + 2;
 
 /* btns */
@@ -93,30 +168,30 @@ const int btnStartNum = 31;
 
 const int btnPin_1 = btnStartNum + 0;
 int btnRead_1 = 0;
-ModuleSet ModuleSet_1(servo_1, btnPin_1); 
+ModuleSet ModuleSet_1(servo_1, btnPin_1);
 
 const int btnPin_2 = btnStartNum + 1;
 int btnRead_2 = 0;
-ModuleSet ModuleSet_2(servo_2, btnPin_2); 
+ModuleSet ModuleSet_2(servo_2, btnPin_2);
 
 const int btnPin_3 = btnStartNum + 2;
 int btnRead_3 = 0;
-ModuleSet ModuleSet_3(servo_3, btnPin_3); 
+ModuleSet ModuleSet_3(servo_3, btnPin_3);
 
 /* others */
-int pos = 0;   
+int pos = 0;
 
 
 /*
- * setup 
- */
+   setup
+*/
 void setup() {
-  Serial.begin(9600); 
+  Serial.begin(9600);
 
   // init servos
-  servo_1.attach(servo_1_pin); 
-  servo_2.attach(servo_2_pin); 
-  servo_3.attach(servo_3_pin);   
+  servo_1.attach(servo_1_pin);
+  servo_2.attach(servo_2_pin);
+  servo_3.attach(servo_3_pin);
 
   // init btns
   pinMode(btnPin_1, INPUT);
@@ -125,15 +200,15 @@ void setup() {
 }
 
 /*
- * loop
- */
+   loop
+*/
 void loop() {
-  
-  ModuleSet_1.moveTablet();  
-  ModuleSet_2.moveTablet();  
-  ModuleSet_3.moveTablet();  
-//  ModuleSet_1.updateBtnState();    
-//  ModuleSet_2.updateBtnState();  
-//  ModuleSet_3.updateBtnState();    
+
+  ModuleSet_1.moveTablet();
+  ModuleSet_2.moveTablet();
+  ModuleSet_3.moveTablet();
+  //  ModuleSet_1.updateBtnState();
+  //  ModuleSet_2.updateBtnState();
+  //  ModuleSet_3.updateBtnState();
 
 }
