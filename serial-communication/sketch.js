063f884f74b183be78933f80692ae8b891bade67
vince19972 <vince19972@gmail.com>
2018-12-10 11:16:21 -0500

    [p5] refactor

--- serial-communication/sketch.js
+++ serial-communication/sketch.js
@@ -3,7 +3,7 @@
 */
 
 var serial
-var portName = '/dev/cu.usbmodem14621'
+var portName = '/dev/cu.usbmodem144101'
 
 var store = {
   currentState: 'sleeping',
@@ -13,7 +13,7 @@ var store = {
   uploadLastTime: 0,
   showSequence: false,
   seqLastTime: 0, //for blinking deactivating sequence
-  seqLit:false
+  seqLit: false
 }
 var flags = {
   isPlayingSound: false,
@@ -28,16 +28,7 @@ var flags = {
     regular_7: false,
     regular_8: false,
     regular_9: false,
-    regular_final: false,
-    // advanced_1: false,
-    // advanced_2: false,
-    // advanced_3: false,
-    // advanced_4: false,
-    // advanced_5: false,
-    // advanced_6: false,
-    // advanced_7: false,
-    // advanced_8: false,
-    // advanced_9: false,
+    advanced_0: false,
   }
 }
 
@@ -46,11 +37,14 @@ var setter = {
     return data.slice(0, -1)
   },
   activateAction(soundFileObj, toneCallback = false, prevIsPlaying = true) {
-    console.log(store.currentState)
+    // console.log(store.currentState)
     if (!flags.isPlayingSound && !prevIsPlaying && !flags.enteredState[store.currentState]) {
       // default p5 soundfile play
       if (soundFileObj) {
-        console.log(soundFileObj)
+        // update isPlaying flag
+        flags.isPlayingSound = true
+
+        // play sound
         var { soundFile, startTime } = soundFileObj
         if (startTime)
           soundFile.play(startTime)
@@ -58,22 +52,21 @@ var setter = {
           soundFile.play()
 
         // update isPlaying flag
-        update.isPlaying(true)
         soundFile.onended(function () {
-          update.isPlaying(false)
+          flags.isPlayingSound = false
         })
       }
 
       // tone.js callback
       if (toneCallback) toneCallback()
     } else {
-      update.isPlaying(false)
+      flags.isPlayingSound = false
     }
 
     if (!flags.enteredState[store.currentState]) {
-      store.lastTime = millis();
+      store.lastTime = millis()
       if (store.currentState == 'advanced_0') {
-        store.uploadLastTime = millis();
+        store.uploadLastTime = millis()
       }
     }
 
@@ -85,13 +78,11 @@ var setter = {
   },
   deactivateAction(soundFile = false, toneCallback = false, isToPlay = false) {
     console.log(store.currentState)
-    console.log(flags.enteredState[store.currentState])
-    if (!getter.uploadingVoiceIsPlaying() && !flags.enteredState[store.currentState]) {
+    if (!getter.actDeactVoiceIsPlaying() && !getter.uploadingVoiceIsPlaying() && !flags.enteredState[store.currentState]) {
       if (isToPlay) {
         soundFile.play()
       } else {
         if (soundFile.isPlaying()) {
-          console.log('in to play function')
           if (toneCallback) toneCallback()
           if (soundFile) soundFile.stop()
         }
@@ -103,7 +94,6 @@ var setter = {
     // send data back to Arduino
     setter.sendSerialData()
   },
-
   startUrgingTimer() {
     console.log("urging timer starts");
     // console.log(millis() - store.lastTime);
@@ -114,8 +104,6 @@ var setter = {
       store.lastTime = millis();
     }
   },
-
-
   startUploadingTimer() {
     if (millis() - store.uploadLastTime > 7000 && store.counter < uploadingVoice.length) {
       console.log(millis() - store.uploadLastTime);
@@ -131,39 +119,22 @@ var setter = {
       store.uploadLastTime = millis()
     }
   },
-
-
-  // uploadingVoiceIsPlaying() {
-  //   let numOfUploadingVoices = uploadingVoice.filter(_voice => _voice.isPlaying() == true).length
-  //   if (numOfUploadingVoices == 1) {
-  //     return true
-  //   } else if (numOfUploadingVoices == 0) {
-  //     return false
-  //   }
-  // },
-
   sendSerialData() {
-    // const isPlaying = getter.actDeactVoiceIsPlaying()
-    if (getter.actDeactVoiceIsPlaying()) serial.write(72)
+    const isPlaying = getter.actDeactVoiceIsPlaying() || getter.uploadingVoiceIsPlaying() || flags.isPlayingSound
+    console.log(isPlaying)
+    if (isPlaying) serial.write(72)
     else serial.write(66)
-    // if (getter.uploadingVoiceIsPlaying()) serial.write(80) //P
-    // else serial.write(81) //Q
   }
 }
 
 var getter = {
   actDeactVoiceIsPlaying() {
     let numOfActDeactVoices = actDeactVoice.filter(_voice => _voice.isPlaying()).length
-    return numOfActDeactVoices === 1 ? 1 : 0
+    return numOfActDeactVoices >= 1
   },
-
   uploadingVoiceIsPlaying() {
-    let numOfUploadingVoices = uploadingVoice.filter(_voice => _voice.isPlaying() == true).length
-    if (numOfUploadingVoices == 1) {
-      return true
-    } else if (numOfUploadingVoices == 0) {
-      return false
-    }
+    let numOfUploadingVoices = uploadingVoice.filter(_voice => _voice.isPlaying()).length
+    return numOfUploadingVoices >= 1
   }
 }
 
@@ -230,6 +201,7 @@ ampEnv = new Tone.AmplitudeEnvelope({
   'sustain': 0.05,
   'release': 0.05
 }).toMaster()
+
 //noise for interrupting uploading
 var noise = new Tone.Noise('white').connect(ampEnv).start()
 
@@ -260,24 +232,25 @@ osc3.volume.value = -30
 */
 
 function preload() {
-  //activating voice:
-  a0 = loadSound("./sounds/ActivationV3/a0.mp3");
-  a1 = loadSound("./sounds/ActivationV3/a1.mp3");
-  a3 = loadSound("./sounds/ActivationV3/a3.mp3");
-  a5 = loadSound("./sounds/ActivationV3/a5.mp3");
-  a7 = loadSound("./sounds/ActivationV3/a7.mp3");
-  a9 = loadSound("./sounds/ActivationV3/a9.mp3");
-  a10 = loadSound("./sounds/ActivationV3/a10.mp3");
-  exit = loadSound('./sounds/ActivationV3/exit.mp3');
-  urg1 = loadSound("./sounds/ActivationV3/urging 1.mp3");
-  urg2 = loadSound("./sounds/ActivationV3/urging 2.mp3");
-  //deactivating voice
-  d9 = loadSound("./sounds/Deactivation/d9.mp3");
-  d7 = loadSound("./sounds/Deactivation/d7.mp3");
-  d5 = loadSound("./sounds/Deactivation/d5.mp3");
-  d3 = loadSound("./sounds/Deactivation/d3.mp3");
-  d1 = loadSound("./sounds/Deactivation/d1.mp3");
-  d0 = loadSound("./sounds/Deactivation/d0.mp3");
+  // //activating voice:
+  // a0 = loadSound("./sounds/ActivationV3/a0.mp3")
+  // a1 = loadSound("./sounds/ActivationV3/a1.mp3")
+  // a3 = loadSound("./sounds/ActivationV3/a3.mp3")
+  // a5 = loadSound("./sounds/ActivationV3/a5.mp3")
+  // a7 = loadSound("./sounds/ActivationV3/a7.mp3")
+  // a9 = loadSound("./sounds/ActivationV3/a9.mp3")
+  a10 = loadSound("./sounds/ActivationV3/a10.mp3")
+  // exit = loadSound('./sounds/ActivationV3/exit.mp3')
+  // urg1 = loadSound("./sounds/ActivationV3/urging 1.mp3")
+  // urg2 = loadSound("./sounds/ActivationV3/urging 2.mp3")
+
+  // //deactivating voice
+  // d9 = loadSound("./sounds/Deactivation/d9.mp3")
+  // d7 = loadSound("./sounds/Deactivation/d7.mp3")
+  // d5 = loadSound("./sounds/Deactivation/d5.mp3")
+  // d3 = loadSound("./sounds/Deactivation/d3.mp3")
+  // d1 = loadSound("./sounds/Deactivation/d1.mp3")
+  // d0 = loadSound("./sounds/Deactivation/d0.mp3")
 
   // uploading voices files
   u1 = loadSound('./sounds/Uploading/1initialize.mp3')
@@ -287,34 +260,30 @@ function preload() {
   u5 = loadSound('./sounds/Uploading/5uploading.mp3')
   u6 = loadSound('./sounds/Uploading/Congratulations.mp3')
 
-  // // testing voice files
-  // a0 = loadSound('./sounds/ActivationTest/a0.mp3')
-  // a1 = loadSound('./sounds/ActivationTest/a1.mp3')
-  // a3 = loadSound('./sounds/ActivationTest/a3.mp3')
-  // a5 = loadSound('./sounds/ActivationTest/a5.mp3')
-  // a7 = loadSound('./sounds/ActivationTest/a7.mp3')
-  // a9 = loadSound('./sounds/ActivationTest/a9.mp3')
+  // testing voice files
+  a0 = loadSound('./sounds/ActivationTest/a0.mp3')
+  a1 = loadSound('./sounds/ActivationTest/a1.mp3')
+  a3 = loadSound('./sounds/ActivationTest/a3.mp3')
+  a5 = loadSound('./sounds/ActivationTest/a5.mp3')
+  a7 = loadSound('./sounds/ActivationTest/a7.mp3')
+  a9 = loadSound('./sounds/ActivationTest/a9.mp3')
   // a10 = loadSound('./sounds/ActivationTest/a10.mp3')
-  // exit = loadSound('./sounds/ActivationV3/exit.mp3')
+  exit = loadSound('./sounds/ActivationV3/exit.mp3')
 
-  // urg1 = loadSound("./sounds/ActivationTest/urging 1.mp3");
-  // urg2 = loadSound("./sounds/ActivationTest/urging 2.mp3");
-  // d9 = loadSound('./sounds/DeactivationTest/d9.mp3')
-  // d7 = loadSound('./sounds/DeactivationTest/d7.mp3')
-  // d5 = loadSound('./sounds/DeactivationTest/d5.mp3')
-  // d3 = loadSound('./sounds/DeactivationTest/d3.mp3')
-  // d1 = loadSound('./sounds/DeactivationTest/d1.mp3')
-  // d0 = loadSound('./sounds/DeactivationTest/d0.mp3')
+  urg1 = loadSound("./sounds/ActivationTest/urging 1.mp3");
+  urg2 = loadSound("./sounds/ActivationTest/urging 2.mp3");
+  d9 = loadSound('./sounds/DeactivationTest/d9.mp3')
+  d7 = loadSound('./sounds/DeactivationTest/d7.mp3')
+  d5 = loadSound('./sounds/DeactivationTest/d5.mp3')
+  d3 = loadSound('./sounds/DeactivationTest/d3.mp3')
+  d1 = loadSound('./sounds/DeactivationTest/d1.mp3')
+  d0 = loadSound('./sounds/DeactivationTest/d0.mp3')
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight)
   store.showSequence = false
   store.seqLit = false
-
-  /* websocket connection */
-  // socket.onopen = openSocket
-  // socket.onmessage = showData
 
   /* serial port */
   serial = new p5.SerialPort()           	// make a new instance of the serialport library
@@ -331,41 +300,38 @@ function setup() {
   /* sound settings */
   uploadingVoice = [u1, u2, u3, u4, u5, u6]
   uploadingSound = [s11, s12, s13, s14]
-  // actDeactVoice 	= [a0, a1, a3, a5, a7, a9, a10, d9, d7, d5, d3, d1, d0]
   actDeactVoice = [a0, a1, a3, a5, a7, a9, a10, d1]
 
-  //move to start session button click function
-  store.lastTime = millis();
+  /* timer settings */
+  // move to start session button click function
+  store.lastTime = millis()
 
 }
 
 function draw() {
-  background(0);
-  //waiver
-  textSize(20);
-  fill(255, 0, 0);
-  text("waiver:", 10, windowHeight / 2 - 60);
-  fill(255);
-  text("Warning: never take off your headset when the session is in progress, as it might cause irreversible brain damage.", 10, windowHeight / 2 - 30);
-  text("In case of any detected discomfort or malfunction, emergency instruction will light up.", 10, windowHeight / 2);
+  /* p5 canvas sketch */
+  background(0)
+  textSize(20)
+  fill(255, 0, 0)
+  text("waiver:", 10, windowHeight / 2 - 60)
+  fill(255)
+  text("Warning: never take off your headset when the session is in progress, as it might cause irreversible brain damage.", 10, windowHeight / 2 - 30)
+  text("In case of any detected discomfort or malfunction, emergency instruction will light up.", 10, windowHeight / 2)
   if (store.showSequence) {   //blink white and red
     if (millis() - store.seqLastTime > 1000) {
       store.seqLit = !store.seqLit
-      store.seqLastTime = millis();
+      store.seqLastTime = millis()
     }
     if (store.seqLit) {
-      fill(255, 0, 0);
+      fill(255, 0, 0)
     } else {
-      fill(255);
+      fill(255)
     }
-    text("To shut down the program, deactivate the modules in the following sequence: 1, 3, 5, 7, 9, 2, 4, 6, 8, 10", 10, windowHeight / 2 + 30);
+    text("To shut down the program, deactivate the modules in the following sequence: 1, 3, 5, 7, 9, 2, 4, 6, 8, 10", 10, windowHeight / 2 + 30)
   }
-
-  var fmtString = store.currentState
-
-  // console.log(store.currentState)
-
-  switch (fmtString) {
+  console.log(store.currentState)
+  /* p5 sound and main program */
+  switch (store.currentState) {
     case 'sleeping':
       setter.activateAction({
         soundFile: a0,
@@ -417,19 +383,6 @@ function draw() {
       }, false, a7.isPlaying())
       break
     // case 'regular_final':
-    //   setter.activateAction({
-    //     soundFile: a10,
-    //     startTime: 3
-    //   }, () => {
-    //     s10.start()
-    //     s1.stop()
-    //     s2.stop()
-    //     s3.stop()
-    //     s4.stop()
-    //     s5.stop()
-    //     s6.stop()
-    //     s7.stop()
-    //   }, a9.isPlaying())
     //   break
     case 'regular_deactivated':
       console.log('regular deactivated')
@@ -449,7 +402,7 @@ function draw() {
     case 'advanced_0':
       setter.activateAction({
         soundFile: a10,
-        startTime: 3
+        startTime: false
       }, () => {
         s10.start()
         s1.stop()
@@ -460,11 +413,7 @@ function draw() {
         s6.stop()
         s7.stop()
       }, a9.isPlaying())
-      flags.enteredState.regular_final = true
-      if(!a10.isPlaying()){
-        store.showSequence = true;
-      }
-
+      flags.enteredState.advanced_0 = true
       break
     case 'advanced_1':
       if (!a10.isPlaying() && !getter.uploadingVoiceIsPlaying() && !flags.enteredState[store.currentState]) {
@@ -506,7 +455,7 @@ function draw() {
       break
     case 'advanced_deactivated':
       setter.deactivateAction(d0, false, true)
-      store.showSequence = false;
+      store.showSequence = false
       //sound looping
       // console.log('in d0 tone')
       // s11.stop()
@@ -525,22 +474,18 @@ function draw() {
       break
   }
 
-  // console.log(flags.isPlayingSound)
-  // if (store.currentState == 'sleeping') serial.write(72)
-  // else if (store.currentState == 'regular_1') serial.write(66)
-  // else serial.write(65)
-
-
-  //once entering advanced_1 && a10 stops, trigger uploading voice timer,
+  // once entering advanced_1 && a10 stops, trigger uploading voice timer,
   if ((store.currentState == 'advanced_0' && !a10.isPlaying()) || store.currentState == 'advanced_1' || store.currentState == 'advanced_2' || store.currentState == 'advanced_3' || store.currentState == 'advanced_4' || store.currentState == 'advanced_5' || store.currentState == 'advanced_6' || store.currentState == 'advanced_7' || store.currentState == 'advanced_8' || store.currentState == 'advanced_final') {
     setter.startUploadingTimer();
   }
 
-  //if(currentstate = regular 1-8 && currentState !==lastState)
+  // if(currentstate = regular 1-8 && currentState !==lastState)
   if (store.currentState == 'regular_1' || store.currentState == 'regular_2' || store.currentState == 'regular_3' || store.currentState == 'regular_4' || store.currentState == 'regular_5' || store.currentState == 'regular_6' || store.currentState == 'regular_7' || store.currentState == 'regular_8' && store.currentState !== lastState) {
     // store.lastTime = millis()
     setter.startUrgingTimer()
   }
+
+  /* reset lastState */
   lastState = store.currentState
 }
 
@@ -558,12 +503,12 @@ function printList(portList) {
   // portList is an array of serial port names
   for (var i = 0; i < portList.length; i++) {
     // Display the list the console:
-    console.log(i + " " + portList[i]);
+    console.log(i + " " + portList[i])
   }
 }
 
 function serverConnected() {
-  console.log('connected to server.');
+  console.log('connected to server.')
 }
 
 function portOpen() {
@@ -571,21 +516,22 @@ function portOpen() {
 }
 
 function serialEvent() {
-  var inString = serial.readStringUntil('\r\n');
+  var inString = serial.readStringUntil('\r\n')
   if (inString.length > 0) {
     if (inString !== 'hello') {
       var readValue = split(inString, ',')[0]
+      console.log(readValue)
       update.currentState(readValue)
       // console.log(readValue)
-      var sensors = split(inString, ',');           // split the string on the commas
+      var sensors = split(inString, ',')           // split the string on the commas
     }
   }
 }
 
 function serialError(err) {
-  console.log('Something went wrong with the serial port. ' + err);
+  console.log('Something went wrong with the serial port. ' + err)
 }
 
 function portClose() {
-  console.log('The serial port closed.');
+  console.log('The serial port closed.')
 }
